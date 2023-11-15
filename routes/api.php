fcd00c548e3757cfe368cfeeffbf3a674e96c4fa
alvescy95 <changyuen95@gmail.com>
2023-11-16 00:34:40 +0800

    move notification out of me install telescope

--- routes/api.php
+++ routes/api.php
@@ -71,7 +71,7 @@ Route::namespace('API')->group(function () {
             Route::get('','MeController@me');
             Route::post('','MeController@update');
             Route::delete('','MeController@destroy');
-            
+
             Route::prefix('tickets')->group(function () {
                 Route::get('','TicketController@index');
                 Route::get('{id}','TicketController@show');
@@ -102,11 +102,11 @@ Route::namespace('API')->group(function () {
                 Route::post('','VerifyProfileController@store');
             });
 
-            Route::prefix('notifications')->group(function () {
-                Route::get('','NotificationController@index');
-                Route::post('mark-as-read/{id}','NotificationController@markAsRead');
+        });
 
-            });
+        Route::prefix('notifications')->group(function () {
+            Route::get('','NotificationController@index');
+            Route::post('mark-as-read/{id}','NotificationController@markAsRead');
 
         });
 
