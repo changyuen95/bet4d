d5f3337f54888c640dfda0887fa0bb62d0024085
Zhi Hao <zhihaokin1999@gmail.com>
2023-12-18 18:04:06 +0800

    locale

---
+++ app/Http/Controllers/API/LocaleController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Lang;
+class LocaleController extends Controller
+{
+    public function __invoke()
+    {
+        return [
+            'en' => [
+                'translation' => Lang::get('messages', [], 'en')
+            ],
+            'cn' => [
+                'translation' => Lang::get('messages', [], 'cn')
+            ],
+        ];
+    }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
