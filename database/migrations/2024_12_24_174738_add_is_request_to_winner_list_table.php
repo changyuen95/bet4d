4d2893454ba9a6a5cc030af9d5d84054c8332bcd
alvescy95 <changyuen95@gmail.com>
2024-12-25 15:37:59 +0800

    name typo

--- database/migrations/2024_12_24_174738_add_is_request_to_winner_list_table.php
+++ database/migrations/2024_12_24_174738_add_is_request_to_winner_list_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('winner_list', function (Blueprint $table) {
+        Schema::table('winner_lists', function (Blueprint $table) {
             //
             $table->boolean('is_request')->default(false);
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('winner_list', function (Blueprint $table) {
+        Schema::table('winner_lists', function (Blueprint $table) {
             //
             $table->dropColumn('is_request');
         });
