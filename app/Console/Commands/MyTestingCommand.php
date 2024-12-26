0581398e0ffb23e94b67a711ccffc9dd6358234c
alvescy95 <changyuen95@gmail.com>
2024-12-27 00:31:22 +0800

    update

--- app/Console/Commands/MyTestingCommand.php
+++ app/Console/Commands/MyTestingCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Models\Admin;
+use App\Models\Outlet;
+use App\Models\TempAdmin;
 use App\Models\WinnerList;
 use Illuminate\Console\Command;
 
@@ -27,25 +29,32 @@ class MyTestingCommand extends Command
      */
     public function handle()
     {
-        //
+        // create admins for each outlet
 
-        // $distributed_winner_list = WinnerList::where('is_distribute', 1)->whereHas('ticketNumber', function($q){
-        //     $q->whereHas('ticket', function($q){
-                
-        //     });
-        // })->where('outlet_id','01hdfv5e9k4axefw12hwk52y8w')->with('drawResult','ticketNumber','winner');
+        $outlets = Outlet::all();
+        $count = 1;
+        foreach($outlets as $outlet) {
+            $admin = Admin::create([
+                $password = substr(str_shuffle(str_repeat('ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789', 10)), 0, 10),
+                'name' => 'Staff '.$count,
+                'username' => 'staff'.$count,
+                'email' => 'support@fortknox.group',
+                'password' => bcrypt($password),
+                'outlet_id' => $outlet->id,
+                'role' => 'operator',
+                'phone_e164' => '-',
+            ]);
+            $count++;
+            $admin->assignRole('operator');
 
-        // $distributed_winner_list = WinnerList::where('is_distribute', 1)
-        //     ->whereHas('ticketNumber.ticket.outlet', function($q){
-        //         $q->where('outlets.id', '01hdfv5e9k4axefw12hwk52y8w');
-        //     })
-        //     ->with('drawResult', 'ticketNumber', 'winner')
-        //     ->get();
-        
-        $winner= WinnerList::where('action_by', '01hqwgalqwgqtew9vj12w00')
-                            ->where('id', '01hevst6g0xt7cc003ssx1t6vs')
-                            ->with('drawResult', 'ticketNumber', 'winner')
-                            ->first();
-        dd($winner);
+            $new_admin = TempAdmin::create([
+                'username' => $admin->username,
+                'password' => $password,
+                'outlet_name' => $outlet->name,
+            ]);
+        }
+
+
+
     }
 }
